3f2c74cb8ee665638af90f2557147a93a2426e97
Manelsc94 <166518212+Manelsc94@users.noreply.github.com>
2024-10-17 14:27:04 +0100

    Add files via upload

---
+++ minishell.h
@@ -0,0 +1,10 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+#include <stdio.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+#endif
