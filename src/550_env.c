8a5a21086efd4ca600a54deabb7866feb02331bc
Manelsc94 <166518212+Manelsc94@users.noreply.github.com>
2024-10-31 19:01:42 +0000

    Update 550_env.c

--- src/550_env.c
+++ src/550_env.c
@@ -1,32 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   env.c                                              :+:      :+:    :+:   */
+/*   550_env.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: luigi <luigi@student.42porto.com>          +#+  +:+       +#+        */
+/*   By: mde-agui <mde-agui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 10:32:18 by luigi             #+#    #+#             */
-/*   Updated: 2024/10/24 10:32:55 by luigi            ###   ########.fr       */
+/*   Updated: 2024/10/31 17:10:43 by mde-agui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void	msh_env(char **envp)
+int	msh_env(char **envp)
 {
 	int		i;
 	int		j;
 
+	if (!envp)
+		return (1);
 	i = 0;
 	while (envp[i])
 	{
 		j = 0;
 		while (envp[i][j])
 		{
-			write(1, &envp[i][j], 1);
+			if (write(1, &envp[i][j], 1) == -1)
+				return (1);
 			j++;
 		}
-		write (1, "\n", 1);
+		if (write (1, "\n", 1) == -1)
+			return (1);
 		i++;
 	}
+	return (0);
 }
