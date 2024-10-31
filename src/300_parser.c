38a09c75b56630964bd8658bc2a3ded61e1686a5
Manelsc94 <166518212+Manelsc94@users.noreply.github.com>
2024-10-31 18:57:24 +0000

    Update 300_parser.c

--- src/300_parser.c
+++ src/300_parser.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   300_parser.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: luigi <luigi@student.42porto.com>          +#+  +:+       +#+        */
+/*   By: mde-agui <mde-agui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 10:28:36 by luigi             #+#    #+#             */
-/*   Updated: 2024/10/31 16:19:20 by luigi            ###   ########.fr       */
+/*   Updated: 2024/10/31 17:29:48 by mde-agui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,42 +14,42 @@
 
 int	to_parse(t_msh *msh, char *line)
 {
-	t_tkn	*tokens;
+	//t_tkn	*tokens;
 	int		i;
 
-	tokens = NULL;
+	//tokens = NULL;
 	if (!line)
 		return (FAILURE);
 	add_history(line);
 	msh->cmds = ft_calloc(1, sizeof(t_command));
 	if (!msh->cmds)
 		return (FAILURE);
-	msh->cmds->av = split_input(line);
+	msh->cmds->av = split_input(line, msh);
 	if (!msh->cmds->av)
 	{
-		free_cmds(msh->cmds, 0);
+		//free_cmds(msh->cmds, 0);
 		return (FAILURE);
 	}
 	msh->cmd_count = ft_matrixlen(msh->cmds->av);
-	tokens = tokenizer(msh->cmds->av);
+	//tokens = tokenizer(msh->cmds->av);
 	
 	i = -1;
 	while (msh->cmds->av[++i])
 		printf("Arg number [%d] -> %s\n", i, msh->cmds->av[i]);
 	if (msh->cmds->av[0] && ft_strcmp(msh->cmds->av[0], "exit") == 0)
-			msh->exit_status = msh_exit(msh->cmds->av, msh);
+		msh->exit_status = msh_exit(msh->cmds->av, msh);
 	if (msh->cmds->av[0] && ft_strcmp(msh->cmds->av[0], "pwd") == 0 && ft_strlen(msh->cmds->av[0]) == 3)
-		msh_pwd();
+		msh->exit_status = msh_pwd();
 	if (msh->cmds->av[0] && ft_strcmp(msh->cmds->av[0], "echo") == 0)
-		msh_echo(msh->cmds->av);
+		msh->exit_status = msh_echo(msh->cmds->av);
 	if (msh->cmds->av[0] && ft_strcmp(msh->cmds->av[0], "cd") == 0)
-		msh_cd(msh->cmds->av);
+		msh->exit_status = msh_cd(msh->cmds->av);
 	if (msh->cmds->av[0] && ft_strcmp(msh->cmds->av[0], "export") == 0)
-		msh_export(msh->envp);
+		msh->exit_status = msh_export(msh->envp);
 	if (msh->cmds->av[0] && ft_strcmp(msh->cmds->av[0], "env") == 0)
-		msh_env(msh->envp);
+		msh->exit_status = msh_env(msh->envp);
 	if (msh->cmds->av[0] && ft_strcmp(msh->cmds->av[0], "unset") == 0)
-		msh_unset(msh->cmds->av, &(msh->envp));
+		msh->exit_status = msh_unset(msh->cmds->av, &(msh->envp));
 	if (line)
 	{
 		free(line);
@@ -59,4 +59,3 @@ int	to_parse(t_msh *msh, char *line)
 	free(msh->cmds);
 	return (SUCCESS);
 }
-
