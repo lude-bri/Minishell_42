80c4ce37415e7ba5991afb2e7a37977176187435
Manelsc94 <166518212+Manelsc94@users.noreply.github.com>
2024-10-31 18:58:57 +0000

    Update minishell.h

--- includes/minishell.h
+++ includes/minishell.h
@@ -1,6 +1,7 @@
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
+# define _GNU_SOURCE
 
 /* ************************************************************************** */
 /*                                  INCLUDES                                  */
