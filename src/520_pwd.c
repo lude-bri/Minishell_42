6f34e74d87ef61fd1e3d70bb1efa26783b154e8c
Manelsc94 <166518212+Manelsc94@users.noreply.github.com>
2024-10-31 19:00:42 +0000

    Update 520_pwd.c

--- src/520_pwd.c
+++ src/520_pwd.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pwd.c                                              :+:      :+:    :+:   */
+/*   520_pwd.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: luigi <luigi@student.42porto.com>          +#+  +:+       +#+        */
+/*   By: mde-agui <mde-agui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 12:49:08 by luigi             #+#    #+#             */
-/*   Updated: 2024/10/23 12:49:09 by luigi            ###   ########.fr       */
+/*   Updated: 2024/10/31 17:10:02 by mde-agui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void	msh_pwd(void)
+int	msh_pwd(void)
 {
 	char	*cwd;
 
@@ -21,7 +21,12 @@ void	msh_pwd(void)
 	{
 		ft_printf("%s\n", cwd);
 		free(cwd);
+		return (1);
 	}
 	else
+	{
 		perror("pwd");
+		return (1);
+	}
+	return (0);
 }
