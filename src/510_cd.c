3533597900aa72d50ee4e4ab5dc94a31498daed8
Manelsc94 <166518212+Manelsc94@users.noreply.github.com>
2024-10-31 19:00:14 +0000

    Update 510_cd.c

--- src/510_cd.c
+++ src/510_cd.c
@@ -3,24 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   510_cd.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: luigi <luigi@student.42porto.com>          +#+  +:+       +#+        */
+/*   By: mde-agui <mde-agui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 12:48:25 by luigi             #+#    #+#             */
-/*   Updated: 2024/10/26 13:02:19 by luigi            ###   ########.fr       */
+/*   Updated: 2024/10/31 17:09:39 by mde-agui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void	msh_cd(char **argv)
+int	msh_cd(char **argv)
 {
 	if (argv[1] == NULL)
 	{
 		printf("cd: missing argument\n");
-		return ;
+		return (1);
 	}
 	if (chdir(argv[1]) != 0)
+	{
 		perror("cd error\n");
+		return (1);
+	}
+	return (0);
 }
 //
 // static int	change_directory(t_msh *msh, char *path)
