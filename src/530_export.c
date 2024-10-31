ded2437874a3865a617c57622452b5e51a395104
Manelsc94 <166518212+Manelsc94@users.noreply.github.com>
2024-10-31 19:01:00 +0000

    Update 530_export.c

--- src/530_export.c
+++ src/530_export.c
@@ -1,23 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   export.c                                           :+:      :+:    :+:   */
+/*   530_export.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: luigi <luigi@student.42porto.com>          +#+  +:+       +#+        */
+/*   By: mde-agui <mde-agui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 12:49:02 by luigi             #+#    #+#             */
-/*   Updated: 2024/10/23 12:49:03 by luigi            ###   ########.fr       */
+/*   Updated: 2024/10/31 17:10:14 by mde-agui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void	msh_export(char **envp)
+int	msh_export(char **envp)
 {
 	int		i;
 	int		j;
 	char	*equal_sign;
 
+	if (!envp)
+		return (1);
 	i = 0;
 	while (envp[i])
 	{
@@ -28,11 +30,13 @@ void	msh_export(char **envp)
 			ft_printf("declare -x ");
 			while (&envp[i][j] != equal_sign)
 			{
-				write(1, &envp[i][j], 1);
+				if (write(1, &envp[i][j], 1) == -1)
+					return (1);
 				j++;
 			}
 			ft_printf("=\"%s\"\n", equal_sign + 1);
 		}
 		i++;
 	}
+	return (0);
 }
