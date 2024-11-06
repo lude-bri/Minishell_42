104ec6d0fc6fc8d7b2f5bd7ce608d3d0af5b3f75
Luigi De Brito Piantavinha <szochoralewizc@hotmail.com>
2024-11-06 07:51:16 +0000

    execute with ./ and /bin/cmd

--- src/400_execute.c
+++ src/400_execute.c
@@ -80,33 +80,66 @@ static void	execute(char **cmd, t_msh *msh, t_tkn *tokens)
 	}
 }
 
-
-static char	*find_path(char *cmd, char **envp)
+static char *find_path(char *cmd, char **envp)
 {
-	char	**full_path;
-	char	*half_path;
-	char	*path;
-	int		i;
+    char **full_path;
+    char *half_path;
+    char *path;
+    int i;
 
-	i = 0;
-	if (!envp[i])
-		return (NULL);
-	while (ft_strnstr(envp[i], "PATH=", 5) == 0)
-		i++;
-	full_path = ft_split(envp[i] + 5, ':');
-	i = 0;
-	while (full_path[i])
-	{
-		half_path = ft_strjoin(full_path[i], "/");
-		path = ft_strjoin(half_path, cmd);
-		free(half_path);
-		if (access(path, F_OK | X_OK) == 0)
-			return (free_arg(full_path), path);
-		free(path);
-		i++;
-	}
-	return (free_arg(full_path), NULL);
+    if (cmd[0] == '/' || (cmd[0] == '.' && cmd[1] == '/'))
+    {
+        if (access(cmd, F_OK | X_OK) == 0)
+            return (ft_strdup(cmd));
+        return (NULL);
+    }
+    i = 0;
+    while (envp[i] && ft_strnstr(envp[i], "PATH=", 5) == 0)
+        i++;
+    if (!envp[i])
+        return (NULL);
+
+    full_path = ft_split(envp[i] + 5, ':');
+    i = 0;
+    while (full_path[i])
+    {
+        half_path = ft_strjoin(full_path[i], "/");
+        path = ft_strjoin(half_path, cmd);
+        free(half_path);
+        if (access(path, F_OK | X_OK) == 0)
+            return (free_arg(full_path), path);
+        free(path);
+        i++;
+    }
+    return (free_arg(full_path), NULL);
 }
+
+// static char	*find_path(char *cmd, char **envp)
+// {
+// 	char	**full_path;
+// 	char	*half_path;
+// 	char	*path;
+// 	int		i;
+//
+// 	i = 0;
+// 	if (!envp[i])
+// 		return (NULL);
+// 	while (ft_strnstr(envp[i], "PATH=", 5) == 0)
+// 		i++;
+// 	full_path = ft_split(envp[i] + 5, ':');
+// 	i = 0;
+// 	while (full_path[i])
+// 	{
+// 		half_path = ft_strjoin(full_path[i], "/");
+// 		path = ft_strjoin(half_path, cmd);
+// 		free(half_path);
+// 		if (access(path, F_OK | X_OK) == 0)
+// 			return (free_arg(full_path), path);
+// 		free(path);
+// 		i++;
+// 	}
+// 	return (free_arg(full_path), NULL);
+// }
 
 /////////////////////////////////
 ////         OLD EXEC        ////
