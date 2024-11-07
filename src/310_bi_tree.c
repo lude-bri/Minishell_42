ab4fd55be7ee404d5bb20486de848e7ca782a2d5
Luigi De Brito Piantavinha <szochoralewizc@hotmail.com>
2024-11-07 10:40:59 +0000

    bin_tree

--- src/310_bi_tree.c
+++ src/310_bi_tree.c
@@ -6,7 +6,7 @@
 /*   By: luigi <luigi@student.42porto.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 10:21:43 by luigi             #+#    #+#             */
-/*   Updated: 2024/11/07 10:22:17 by luigi            ###   ########.fr       */
+/*   Updated: 2024/11/07 10:40:08 by luigi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,18 @@
 
 t_tkn	*create_bin_tree(t_tkn *tokens)
 {
+	t_tkn	*current;
+
+	if (!tokens)
+		return (NULL);
+	current = tokens;
+	while (current && current->type != TKN_PIPE)
+		current = current->next;
+	if (current && current->type == TKN_PIPE)
+	{
+		current->left = tokens;
+		current->right = create_bin_tree(current->next);
+		tokens = current;
+	}
 	return (tokens);
 }
