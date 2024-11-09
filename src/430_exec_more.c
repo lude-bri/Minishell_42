6e21ce925f1d1f66743719e0eb60e870de55914d
Luigi De Brito Piantavinha <szochoralewizc@hotmail.com>
2024-11-09 19:22:58 +0000

    pipes working but leaks

--- src/430_exec_more.c
+++ src/430_exec_more.c
@@ -12,24 +12,6 @@
 
 #include "../includes/minishell.h"
 
-// static void	free_everything_exit(t_tkn *tokens, int exit_code)
-// {
-// 	free_tokens(tokens);
-// 	exit(exit_code);
-// }
-//
-// static void	treat_pipes(t_tkn *tokens, int status)
-// {
-// 	if (WIFEXITED(status))
-// 		free_everything_exit(tokens, WEXITSTATUS(status));
-// 	else if (WIFSIGNALED(status))
-// 	{
-// 		if (WTERMSIG(status) == SIGINT)
-// 			write(2, "\n", 1);
-// 		free_everything_exit(tokens, 128 + WTERMSIG(status));
-// 	}
-// }
-//
 static void	apply_pipe(t_tkn *dir, t_msh *msh, int *fd,
 		int multipurp_fd)
 {
@@ -37,6 +19,7 @@ static void	apply_pipe(t_tkn *dir, t_msh *msh, int *fd,
 	{
 		close(fd[0]);
 		close(fd[1]);
+		exit(127);
 	}
 	close(fd[0]);
 	close(fd[1]);
@@ -54,23 +37,66 @@ static int	exec_pipe(t_msh *msh, t_tkn *tokens)
 		return (FAILURE);
 	pid_left = fork();
 	if (pid_left == -1)
+	{
 		ft_close(fd);
+		return (FAILURE);
+	}
 	else if (pid_left == 0)
+	{
+		close(fd[0]);
 		apply_pipe(tokens->left, msh, fd, 1);
-	printf("here");
+		exit(EXIT_SUCCESS);
+	}
 	pid_right = fork();
 	if (pid_right == -1)
-		ft_close(fd);
+	{
+		perror("fork failed");
+		close(fd[0]);
+		close(fd[1]);
+		return (FAILURE);
+	}
 	else if (pid_right == 0)
+	{
+		close(fd[1]);
 		apply_pipe(tokens->right, msh, fd, 0);
+		exit(EXIT_SUCCESS);
+	}
 	close(fd[0]);
 	close(fd[1]);
-	free_msh(NULL, msh, tokens);
 	waitpid(pid_left, &status, 0);
 	waitpid(pid_right, &status, 0);
-	// treat_pipes(tokens, status);
 	return (SUCCESS);
 }
+
+
+// static int	exec_pipe(t_msh *msh, t_tkn *tokens)
+// {
+// 	int		fd[2];
+// 	pid_t	pid_left;
+// 	pid_t	pid_right;
+// 	int		status;
+//
+// 	if (pipe(fd) < 0)
+// 		return (FAILURE);
+// 	pid_left = fork();
+// 	if (pid_left == -1)
+// 		ft_close(fd);
+// 	else if (pid_left == 0)
+// 		apply_pipe(tokens->left, msh, fd, 1);
+// 	printf("here");
+// 	pid_right = fork();
+// 	if (pid_right == -1)
+// 		ft_close(fd);
+// 	else if (pid_right == 0)
+// 		apply_pipe(tokens->right, msh, fd, 0);
+// 	close(fd[0]);
+// 	close(fd[1]);
+// 	free_msh(NULL, msh, tokens);
+// 	waitpid(pid_left, &status, 0);
+// 	waitpid(pid_right, &status, 0);
+// 	// treat_pipes(tokens, status);
+// 	return (SUCCESS);
+// }
 
 int	exec_more(t_msh *msh, t_tkn *tokens)
 {
