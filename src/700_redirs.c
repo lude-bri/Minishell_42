6481855f30dad2080d7a244718342d43e368662f
Luigi Piantavinha <szochoralewizc@hotmail.com>
2024-11-20 15:27:53 +0000

    verifying flags

--- src/700_redirs.c
+++ src/700_redirs.c
@@ -12,11 +12,24 @@
 
 #include "../includes/minishell.h"
 
+static void	flags_check(t_tkn *tokens)
+{
+	if (ft_strchr(tokens->next->next->name, '-') != 0)
+		printf("and it is exec\n");
+	else
+		printf("and it is not exec\n");
+}
+
 void	redirs(t_tkn *tokens, t_msh *msh)
 {
 	t_tkn	*tkn_exec;
 
 	tkn_exec = tokens;
+	if (tokens->next->next != NULL && tokens->next->next->type != TKN_PIPE)
+	{	
+		printf("there is something\n");
+		flags_check(tokens);
+	}
     if (tokens->type == TKN_IN)
 		redir_in(tkn_exec, msh);
     else if (tokens->type == TKN_OUT)
