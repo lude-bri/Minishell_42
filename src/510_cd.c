f3a07a700801a02c6a4d075a69e483242034f01c
Manelsc94 <166518212+Manelsc94@users.noreply.github.com>
2024-12-16 19:01:43 +0000

    Update 510_cd.c

--- src/510_cd.c
+++ src/510_cd.c
@@ -12,6 +12,100 @@
 
 #include "../includes/minishell.h"
 
+int find_env_var(char **envp, const char *var)
+{
+    int i = 0;
+    size_t var_len = strlen(var);
+    
+    while (envp[i])
+    {
+        if (strncmp(envp[i], var, var_len) == 0 && envp[i][var_len] == '=')
+            return (i);
+        i++;
+    }
+    return (-1);
+}
+
+// Helper function to update environment variable
+void update_env_var(char **envp, const char *var, const char *value)
+{
+    int index;
+    char *new_var;
+    
+    // Calculate the length needed and allocate
+    new_var = malloc(strlen(var) + strlen(value) + 2); // +2 for '=' and '\0'
+    if (!new_var)
+        return;
+        
+    // Create the new environment string
+    sprintf(new_var, "%s=%s", var, value);
+    
+    // Find if variable already exists
+    index = find_env_var(envp, var);
+    
+    if (index != -1)
+    {
+        // Free the old string and replace it
+        free(envp[index]);
+        envp[index] = new_var;
+    }
+}
+
+int msh_cd(char **argv, char **envp)
+{
+    char current_path[PATH_MAX];
+    char new_path[PATH_MAX];
+    
+    if (!argv || !envp)
+        return (1);
+        
+    // Get current path before changing directory
+    if (getcwd(current_path, sizeof(current_path)) == NULL)
+    {
+        perror("msh: getcwd");
+        return (1);
+    }
+    
+    // Handle cd argument checks
+    if (argv[1] == NULL)
+    {
+        char *home = getenv("HOME");
+        if (!home)
+        {
+            printf("msh: cd: HOME not set\n");
+            return (1);
+        }
+        if (chdir(home) != 0)
+        {
+            perror("msh: cd");
+            return (1);
+        }
+    }
+    else if (argv[2])
+    {
+        printf("msh: cd: too many arguments\n");
+        return (1);
+    }
+    else if (chdir(argv[1]) != 0)
+    {
+        perror("msh: cd");
+        return (1);
+    }
+    
+    // Get new current working directory
+    if (getcwd(new_path, sizeof(new_path)) == NULL)
+    {
+        perror("msh: getcwd");
+        return (1);
+    }
+    
+    // Update PWD and OLDPWD in environment
+    update_env_var(envp, "OLDPWD", current_path);
+    update_env_var(envp, "PWD", new_path);
+    
+    return (0);
+}
+
 int	msh_cd(char **argv)
 {
 	if (argv[1] == NULL)
