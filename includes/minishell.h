d8d1b342bbf53c173e922cdb2154eda0def1a5dd
Manelsc94 <166518212+Manelsc94@users.noreply.github.com>
2024-12-23 15:56:32 +0000

    Update minishell.h

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: mde-agui <mde-agui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/21 15:40:26 by mde-agui          #+#    #+#             */
-/*   Updated: 2024/12/21 15:44:27 by mde-agui         ###   ########.fr       */
+/*   Updated: 2024/12/23 03:14:13 by mde-agui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -104,6 +104,8 @@ typedef enum e_cmd_group
 //used in 200_tokenization.c
 typedef struct s_tkn_op
 {
+	int		i;
+	int		j;
 	int		number_words;
 	int		start;
 	char	**argv;
@@ -155,13 +157,16 @@ typedef struct s_msh
 	char		*pwd;
 	char		*prompt;
 	char		*line;
+	char		*word;
 
+	int			pid;
 	int			cmd_count;
 	int			pipe_count;
 	int			len;
 	int			exit_status;
 	bool		flag_redir;
 	t_vector	tokens;
+	size_t		word_size;
 
 }				t_msh;
 
@@ -175,6 +180,9 @@ typedef struct s_exp
 	char	*add_sign;
 	char	*remove_sign;
 	char	*var;
+	char	*var_name;
+	char	*value;
+	char	*result;
 	char	*new_entry;
 	char	*updated_value;
 	char	*existing_value;
@@ -182,6 +190,7 @@ typedef struct s_exp
 	char	*to_remove;
 	char	*found;
 	char	**new_envp;
+	int		start;
 }				t_exp;
 
 /* ************************************************************************** */
@@ -215,16 +224,55 @@ char	*build_prompt(t_msh *msh);
 // TOKENS
 
 //200_tokenization.c
-char	**split_input(const char *input, t_msh *msh);
-char	*handle_single_quotes(const char *input, int *i);
-char	*handle_double_quotes(const char *input, int *i, t_msh *msh);
-int		count_words(const char *input);
-int		is_whitespace(char c);
-char	*expand_var(const char *input, int *i, t_msh *msh);
 char	*copy_word(const char *input, int start, int end);
+int		is_whitespace(char c);
+int		count_words(const char *input);
+void	process_input(const char *input, t_tkn_op *sp, t_msh *msh);
+char	**split_input(const char *input, t_msh *msh);
+
+//201_tkns_handlers.c
+void	handle_input_redir(const char *input, t_tkn_op *sp, t_msh *msh);
+void	handle_output_redir(const char *input, t_tkn_op *sp, t_msh *msh);
+void	handle_pipes(const char *input, t_tkn_op *sp);
+void	handle_expand(const char *input, t_tkn_op *sp, t_msh *msh);
+void	handle_quotes(const char *input, t_tkn_op *sp, t_msh *msh);
+
+//202_tkns_handlers_2.c
+char	*handle_double_quotes(const char *input, int *i, t_msh *msh);
+char	*handle_single_quotes(const char *input, int *i);
+void	handle_words(const char *input, t_tkn_op *sp);
+void	handle_double_in_redir(const char *input, t_tkn_op *sp);
+void	handle_single_in_redir(const char *input, t_tkn_op *sp, t_msh *msh);
+
+//203_tkns_misc.c
+int		quote_helper(const char *input, int i);
+bool	special_char_helper(const char *input, int *i);
+bool	regular_char_helper(const char *input, int *i);
+void	append_char_to_word(const char *input, int *i, t_msh *msh);
+int		take_len(const char *input);
+
+//204_tkns_expander.c
+char	*expand_helper(const char *input, int *i, t_msh *msh);
+char	*expand_var(const char *input, int *i, t_msh *msh);
 
 //210_tkns_type.c
+int		token_len(t_tkn *tokens);
+t_tkn	*tkn_new(t_msh *msh, char *content);
+void	verify_tkn_cmd(t_tkn *token);
+void	assign_tkn(t_tkn *token, char **av, t_msh *msh);
 t_tkn	*tokenizer(t_msh *msh, char **av);
+
+//211_tkns_type_handler.c
+int		verify_quotes(char operator, char *line);
+void	handle_heredoc_token(t_tkn *token, char *line);
+void	handle_other_tokens(t_tkn *tkn, char **av, t_msh *msh);
+
+//212_tkns_type_handler_2.c
+void	handle_blank_and_null(t_tkn *token);
+void	handle_pipe_token(t_tkn *token, char *line);
+void	handle_append_token(t_tkn *token);
+void	handle_out_token(t_tkn *token);
+void	handle_in_token(t_tkn *token);
 
 //220_vectors.c
 void	vector_push(t_vector *vector, t_tkn *token);
@@ -234,8 +282,19 @@ void	vector_push(t_vector *vector, t_tkn *token);
 /* *************** */
 // PARSERS
 
-//300_parser.c	
+//300_parser.c
+int		count_pipes(t_tkn *tokens);
+void	error_syntax(t_msh *msh);
+int		is_operator(char *line);
+int		syntax(t_msh *msh, char *line);
 t_tkn	*to_parse(t_msh *msh);
+
+//301_parser_check.c
+int		check_initial_syntax(t_msh *msh, char *line);
+int		check_str_syntax(t_msh *msh, char *str);
+int		check_line_syntax(t_msh *msh, char *line);
+int		initial_syntax_checks(t_msh *msh, char *line, char *str);
+int		detailed_syntax_checks(t_msh *msh, char *str);
 
 //310_bi_tree.c
 t_tkn	*create_bin_tree(t_tkn *tokens);
@@ -251,14 +310,21 @@ int		syntax_check_pipes(t_msh *msh, t_tkn *tokens);
 // EXECUTE
 
 //400_execute.c
-int		to_execute(t_msh *msh, t_tkn *tokens);
+void	execute_builtin_commands(t_tkn *tokens, t_msh *msh);
 int		exec_bi(t_tkn *tokens, t_msh *msh);
 int		exec_exe(t_tkn *tokens, t_msh *msh);
+void	exec_special(t_tkn *tokens, t_msh *msh);
 void	execute(t_msh *msh, t_tkn *tokens);
-char	*find_path(char *cmd, char **envp);
 
 //401_build_args.c
 char	**build_args(t_tkn *tokens);
+
+//402_execute_misc.c
+int		to_execute(t_msh *msh, t_tkn *tokens);
+void	handle_redirections(t_tkn *tokens, t_msh *msh, int *fd_in, int *fd_out);
+char	*check_direct_path(char *cmd);
+char	*search_in_env_path(char *cmd, char **envp);
+char	*find_path(char *cmd, char **envp);
 
 //410_signals.c
 void	setup_signals(void);
@@ -292,10 +358,20 @@ int		msh_pwd(void);
 //530_export.c
 int		sort_envp(char **envp);
 int		msh_export_no_var(char **envp);
-int		add_new_variable(char ***envp, t_exp *exp, const char *new_var);
-int		update_existing_variable(char ***envp, t_exp *exp);
 int		parse_variable(const char *new_var, t_exp *exp);
 int		msh_export(char ***envp, const char *new_var);
+
+//531_export_change_var.c
+int		add_new_variable(char ***envp, t_exp *exp, const char *new_var);
+int		handle_addition(t_exp *exp);
+int		handle_removal(t_exp *exp);
+int		update_variable_entry(char ***envp, t_exp *exp, int i);
+int		update_existing_variable(char ***envp, t_exp *exp);
+
+//532_ export_misc.c
+void	print_env_var(char *env_var);
+int		find_existing_variable(char ***envp, t_exp *exp);
+int		is_alpha(const char *str);
 
 //540_unset.c
 int		is_variable_match(const char *env_var, const char *var_name);
@@ -305,9 +381,12 @@ int		msh_unset(char **argv, char ***envp);
 int		msh_env(char **envp);
 
 //560_exit.c
-int		is_num(const char *str);
-// int		msh_exit(char **argv, t_msh *msh);
 int		msh_exit(char **argv, t_msh *msh, t_tkn *tokens);
+
+//561_exit_misc.c
+int		is_num(const char *str);
+int		is_letter(const char *str);
+int		is_sign(char *argv);
 
 /* **************** */
 /*      600        */
@@ -315,8 +394,15 @@ int		msh_exit(char **argv, t_msh *msh, t_tkn *tokens);
 // PIPES
 
 //600_pipes.c
-int		exec_pipe(t_msh *msh, t_tkn *tokens);
 void	apply_pipe(t_tkn *dir, t_msh *msh, int *fd, int multi_fd);
+int		setup_pipe_and_fork(t_msh *msh, t_tkn *tokens, int *fd);
+int		exec_pipe(t_msh *msh, t_tkn *tokens);
+
+//601_pipes_child.c
+void	handle_heredoc(t_tkn *tokens, t_msh *msh, int *fd);
+void	handle_left_child(t_tkn *tokens, t_msh *msh, int *fd);
+void	handle_right_child(t_tkn *tokens, t_msh *msh, int *fd);
+void	wait_for_child(t_msh *msh, pid_t pid_left, pid_t pid_right);
 
 /* **************** */
 /*      700        */
@@ -324,8 +410,14 @@ void	apply_pipe(t_tkn *dir, t_msh *msh, int *fd, int multi_fd);
 // REDIRECTS
 
 //700_redirs.c
-int		exec_redirs(t_tkn *tokens, t_msh *msh);
 void	redirs(t_tkn *tokens, t_msh *msh);
+int		exec_redirs(t_tkn *tokens, t_msh *msh);
+
+//701_redirs_2.c
+void	process_redirection(t_tkn *tokens, t_msh *msh);
+void	update_token_types(t_tkn *tokens);
+void	handle_redirs(t_tkn *tokens, t_msh *msh);
+void	handle_command_arguments(t_tkn *tokens, t_msh *msh);
 void	redirs_2(t_tkn *tokens, t_msh *msh);
 
 //710_redir_in.c
@@ -338,7 +430,12 @@ void	redir_out(t_tkn *tokens, t_msh *msh);
 void	redir_append(t_tkn *tokens, t_msh *msh);
 
 //740_heredoc.c
+void	setup_pipe_and_signals(t_tkn *tokens, t_msh *msh, int *pipe_fd);
+void	read_n_write_lines(t_tkn *tokens, t_msh *msh, char *arg, int *pipe_fd);
+void	read_n_write_lines2(t_tkn *tokens, t_msh *msh, char *arg, int *pipe_fd);
 void	heredoc(t_tkn *tokens, t_msh *msh, char *arg, int flag);
+
+//741_heredoc_pipe.c
 int		heredoc_pipe(t_tkn *tokens, t_msh *msh, char *arg, int flag);
 
 /* **************** */
