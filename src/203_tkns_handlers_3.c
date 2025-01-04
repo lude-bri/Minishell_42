a334c9f23cddf804ab8eb3921eb9a0646e68feb2
Manelsc94 <166518212+Manelsc94@users.noreply.github.com>
2025-01-04 22:19:26 +0000

    Update 203_tkns_handlers_3.c

--- src/203_tkns_handlers_3.c
+++ src/203_tkns_handlers_3.c
@@ -6,7 +6,7 @@
 /*   By: mde-agui <mde-agui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/24 11:59:50 by luigi             #+#    #+#             */
-/*   Updated: 2025/01/03 14:15:32 by mde-agui         ###   ########.fr       */
+/*   Updated: 2025/01/04 20:47:51 by mde-agui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,66 +47,92 @@ void	handle_single_out_redir(const char *input, t_tkn_op *sp, t_msh *msh)
 	}
 }
 
-static void	pr_quote(const char *input, t_quote *qd, t_msh *msh, char quote)
+static void    pr_quote(const char *input, t_quote *qd, t_msh *msh, char quote)
 {
-	char	*expanded;
+    char    *expanded;
 
-	if ((*qd->buf_i > 0 || quote == '"') && (*qd->i != 0))
-		(*qd->i)++;
-	while (input[*qd->i] && input[*qd->i] != quote
-		&& !is_whitespace(input[*qd->i]))
-	{
-		if (quote == '"' && input[*qd->i] == '$')
-		{
-			expanded = expand_var(input, qd->i, msh);
-			if (expanded)
-			{
-				ft_strncat(qd->buffer, expanded, BUF_SIZE - *qd->buf_i - 1);
-				*qd->buf_i = ft_strlen(qd->buffer);
-				free(expanded);
-			}
-		}
-		else
-			qd->buffer[(*qd->buf_i)++] = input[(*qd->i)++];
-	}
-	if (*qd->buf_i > 0 || quote == '"')
-	{
-		if (input[*qd->i] == quote)
-			(*qd->i)++;
-	}
+    if ((*qd->buf_i > 0 || quote == '"') && (*qd->i != 0))
+        (*qd->i)++;
+    
+    while (input[*qd->i] && input[*qd->i] != quote)
+    {
+        if (quote == '"' && input[*qd->i] == '$')
+        {
+            expanded = expand_var(input, qd->i, msh);
+            if (expanded)
+            {
+                ft_strncat(qd->buffer, expanded, BUF_SIZE - *qd->buf_i - 1);
+                *qd->buf_i = ft_strlen(qd->buffer);
+                free(expanded);
+            }
+        }
+        else
+        {
+            qd->buffer[(*qd->buf_i)++] = input[(*qd->i)++];
+        }
+    }
+    
+    if (input[*qd->i] == quote)
+        (*qd->i)++;
 }
 
-void	handle_words(const char *input, t_tkn_op *sp, t_msh *msh)
-{
-	char	buffer[BUF_SIZE];
-	int		buf_i;
-	t_quote	qd;
-	char	*expanded;
 
-	ft_memset(buffer, 0, BUF_SIZE);
-	buf_i = 0;
-	while (input[sp->i] && !is_whitespace(input[sp->i])
-		&& input[sp->i] != '|' && input[sp->i] != '>' && input[sp->i] != '<')
-	{
-		if (input[sp->i] == '"' || input[sp->i] == '\'')
-		{
-			qd = (t_quote){buffer, &buf_i, &sp->i};
-			pr_quote(input, &qd, msh, input[sp->i]);
-		}
-		else if (input[sp->i] == '$')
-		{
-			qd = (t_quote){buffer, &buf_i, &sp->i};
-			expanded = expand_var(input, qd.i, msh);
-			if (expanded)
-			{
-				ft_strncat(qd.buffer, expanded, BUF_SIZE - *qd.buf_i - 1);
-				*qd.buf_i = ft_strlen(qd.buffer);
-				free(expanded);
-			}
-		}
-		else
-			buffer[buf_i++] = input[sp->i++];
-	}
-	buffer[buf_i] = '\0';
-	sp->argv[sp->j++] = ft_strdup(buffer);
+// Modified handle_words function
+void    handle_words(const char *input, t_tkn_op *sp, t_msh *msh)
+{
+    char    buffer[BUF_SIZE];
+    int     buf_i;
+    t_quote qd;
+    char    *expanded;
+    bool    is_first_word;
+
+    ft_memset(buffer, 0, BUF_SIZE);
+    buf_i = 0;
+    is_first_word = (sp->j == 0);
+
+    // Special handling for quoted first word
+    if (is_first_word && (input[sp->i] == '"' || input[sp->i] == '\''))
+    {
+        char quote_char = input[sp->i];
+        sp->i++; // Skip opening quote
+        while (input[sp->i] && input[sp->i] != quote_char)
+            buffer[buf_i++] = input[sp->i++];
+        if (input[sp->i] == quote_char)
+            sp->i++; // Skip closing quote
+        buffer[buf_i] = '\0';
+        sp->argv[sp->j++] = ft_strdup(buffer);
+        return;
+    }
+
+    while (input[sp->i] && !is_whitespace(input[sp->i])
+        && input[sp->i] != '|' && input[sp->i] != '>' && input[sp->i] != '<')
+    {
+        if (input[sp->i] == '"' || input[sp->i] == '\'')
+        {
+            qd = (t_quote){buffer, &buf_i, &sp->i};
+            pr_quote(input, &qd, msh, input[sp->i]);
+        }
+        else if (input[sp->i] == '$')
+        {
+            qd = (t_quote){buffer, &buf_i, &sp->i};
+            expanded = expand_var(input, qd.i, msh);
+            if (expanded)
+            {
+                if (!(is_first_word && buf_i == 0 && expanded[0] == '\0'))
+                {
+                    ft_strncat(qd.buffer, expanded, BUF_SIZE - *qd.buf_i - 1);
+                    *qd.buf_i = ft_strlen(qd.buffer);
+                }
+                free(expanded);
+            }
+        }
+        else
+            buffer[buf_i++] = input[sp->i++];
+    }
+
+    if (buf_i > 0 || !is_first_word)
+    {
+        buffer[buf_i] = '\0';
+        sp->argv[sp->j++] = ft_strdup(buffer);
+    }
 }
