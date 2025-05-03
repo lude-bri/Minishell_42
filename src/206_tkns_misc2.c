f3e2559074076be8b0fe532c7e8c88d5bf80e360
Luigi Piantavinha <167509620+lude-bri@users.noreply.github.com>
2025-05-03 23:28:26 +0100

    Doxygen 206_tkns_misc2.c

--- src/206_tkns_misc2.c
+++ src/206_tkns_misc2.c
@@ -12,11 +12,27 @@
 
 #include "../includes/minishell.h"
 
+/**
+ * @brief Checks whether a character is considered whitespace.
+ *
+ * Returns true if the character is a space, tab, or newline.
+ *
+ * @param c The character to evaluate.
+ * @return 1 (true) if the character is whitespace; 0 (false) otherwise.
+ */
 int	is_whitespace(char c)
 {
 	return (c == ' ' || c == '\n' || c == '\t');
 }
 
+/**
+ * @brief Advances the input pointer past any leading whitespace characters.
+ *
+ * Modifies the given pointer to point to the first non-whitespace character.
+ * Useful before parsing tokens or arguments.
+ *
+ * @param input A pointer to the input string pointer to modify.
+ */
 void	skip_whitespace(const char **input)
 {
 	while (**input && is_whitespace(**input))
