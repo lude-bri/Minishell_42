2b41088e350fb12786a7a4102eba93c1942cf5dd
Luigi Piantavinha <167509620+lude-bri@users.noreply.github.com>
2025-05-03 23:50:42 +0100

    Doxygen 421_exec_one_misc.c

--- src/421_exec_one_misc.c
+++ src/421_exec_one_misc.c
@@ -12,6 +12,18 @@
 
 #include "../includes/minishell.h"
 
+/**
+ * @brief Executes a builtin command in a forked child process.
+ *
+ * - Calls `exec_bi()` to run the builtin logic.
+ * - Cleans up memory and file descriptors via `free_and_close_all()`.
+ * - Exits with the current shell exit status.
+ *
+ * This function is called inside a child process created by `handle_builtin_commands()`.
+ *
+ * @param msh Pointer to the shell state.
+ * @param tokens Pointer to the command token.
+ */
 void	handle_child_builtin(t_msh *msh, t_tkn *tokens)
 {
 	exec_bi(tokens, msh);
@@ -19,6 +31,18 @@ void	handle_child_builtin(t_msh *msh, t_tkn *tokens)
 	exit(msh->exit_status);
 }
 
+/**
+ * @brief Waits for a builtin child process and sets the exit status.
+ *
+ * - Waits for the child process to complete using `waitpid`.
+ * - Updates `msh->exit_status` based on exit code or signal.
+ * - Prints a newline if the process was interrupted by `SIGINT`.
+ *
+ * This function is called from the parent after forking a builtin.
+ *
+ * @param pid PID of the child process.
+ * @param msh Pointer to the shell state.
+ */
 void	handle_parent_builtin(int pid, t_msh *msh)
 {
 	int	status;
